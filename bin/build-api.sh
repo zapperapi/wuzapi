064b47b099653156f69695b1d75c21f8cdec6ed5
Jefferson Felix <me@jeffersonfelix.dev>
2026-02-01 10:17:46 -0300

    feat: build image

---
+++ bin/build-api.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+# Load environment variables
+set -a; . ./.env; set +a
+export VERSION=$(git rev-parse --short HEAD)
+
+# Build and push the image to ECR
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 058264349674.dkr.ecr.us-east-1.amazonaws.com
+docker build -t zapperapi/wuzapi .
+docker tag zapperapi/wuzapi:latest $ECR_IMAGE_NAME:latest
+docker tag zapperapi/wuzapi:latest $ECR_IMAGE_NAME:$VERSION
+docker push $ECR_IMAGE_NAME:$VERSION
